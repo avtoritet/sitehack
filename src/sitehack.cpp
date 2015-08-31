a3803d4a9b37ae005ad90d324e5a82d45ee9065c
avtoritet <alex3ller@gmail.com>
2015-08-31 19:57:45 -0400

    Site block
    Blocks www.google.com

--- src/sitehack.cpp
+++ src/sitehack.cpp
@@ -1,11 +1,17 @@
-// sitehack.cpp : Hacks hosts file to block a site.
-#include "stdafx.h"
+// sitehack.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
 #include <fstream>
 
-int main() {
+
+int main(int argc, char* argv[])
+{
 	std::ofstream outfile;
 
 	outfile.open("C:\\Windows\\System32\\drivers\\etc\\hosts", std::ios_base::app);
 	outfile << "\n127.0.0.2 www.google.com";
+	
 	return 0;
 }
+
